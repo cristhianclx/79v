28ec84c04a6938aca0c8e296669167dbf51759bd
Cristhian <cristhianclx@gmail.com>
2026-09-25 21:37:26 -0500

    04

--- 04/main.py
+++ 04/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.sql import func
@@ -14,9 +14,11 @@ migrate = Migrate(app, db)
 class User(db.Model):
     __tablename__ = "users"
     
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    code = db.Column(db.String(11), nullable=False)
     first_name = db.Column(db.String(128), nullable=False)
     last_name = db.Column(db.String(128), nullable=False)
+    age = db.Column(db.Integer, nullable=True)
     created = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
@@ -49,8 +51,28 @@ def view_health():
     }
 
 
+@app.route("/users")
+def view_users():
+    items = User.query.all()
+    return render_template("users.html", items=items)
+
+
+@app.route("/users/add", methods=["GET", "POST"])
+def view_users_add():
+    if request.method == "GET":
+        return render_template("users-add.html")
+    if request.method == "POST":
+        item = User(
+            code=request.form["code"],
+            first_name=request.form["first_name"],
+            last_name=request.form["last_name"],
+            age=request.form["age"],
+        )
+        db.session.add(item)
+        db.session.commit()
+        return render_template("users-add.html", message="User saved")
+
+
 # LABORATORIO
-# User, agrega 2 datos en User: code String(11), age Integer
-# genera la migracion
-# aplica la migracion
-# mandas pantallazo archivos y el sqlite
+# /messages # mostrar los mensajes en una tabla
+# /messages/add # agregar un mensaje
